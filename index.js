950675209bbeeb3e7581ec3310df25123ac285eb
Stefan Kleeschulte <stefan.kleeschulte@smk.biz>
2019-07-15 18:01:12 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/**
+ * RFC3986 compliant encodeURIComponent function.
+ * Source: https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+ *
+ * @param {string}  string  String to encode.
+ * @returns {string} Encoded string.
+ */
+function encodeURIComponentRFC3986(string) {
+    return encodeURIComponent(string).replace(/[!'()*]/g, function(c) {
+        return '%' + c.charCodeAt(0).toString(16);
+    });
+}
+
+/**
+ * Get request URL from HTTP response. The hash portion of a request URL is not sent to the server and will not be
+ * included in the result of this function.
+ *
+ * @param {http.IncomingMessage}  res          HTTP response.
+ * @param {boolean}               includeAuth  Whether to include authorization information in the URL.
+ * @returns {URL} The request URL.
+ */
+function getRequestUrl(res, includeAuth) {
+    let url = '';
+
+    // protocol (res.req.agent.protocol is an undocumented feature)
+    url += res.req.agent.protocol + '//';
+
+    // authorization data
+    const authorizationHeader = res.req.getHeader('Authorization');
+    if (includeAuth && authorizationHeader) {
+        const [scheme, param] = authorizationHeader.split(/\s+/);
+
+        if (scheme.toLowerCase() === 'basic') {
+            const decoded = Buffer.from(param, 'base64').toString();
+            const colonPos = decoded.indexOf(':');
+
+            username = (colonPos !== -1) ? decoded.substr(0, colonPos) : decoded;
+            password = (colonPos !== -1) ? decoded.substr(colonPos + 1) : false;
+
+            url += encodeURIComponentRFC3986(username);
+            url += password ? ':' + encodeURIComponentRFC3986(password) : '';
+            url += '@';
+        }
+    }
+
+    // hostname and port
+    url += res.req.getHeader('Host');
+
+    // path
+    url += res.req.path;
+
+    return new URL(url);
+}
+
+module.exports = getRequestUrl;
---
+++ test/test.js
@@ -0,0 +1,96 @@
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const assert = require('assert');
+const getRequestUrl = require('../index');
+
+const HTTP_PORT = 3080;
+const HTTPS_PORT = 3443;
+
+/**
+ * Make HTTP request.
+ *
+ * @param {string}  url  Request URL.
+ * @returns {Promise<http.IncomingMessage>} HTTP response.
+ */
+function request(url) {
+    const isHttps = new URL(url).protocol === 'https:';
+
+    return new Promise(resolve => {
+        if (isHttps) {
+            const options = {
+                ca: fs.readFileSync('test/fixtures/localhost.cert')
+            };
+            https.get(url, options, resolve);
+        } else {
+            http.get(url, resolve);
+        }
+    });
+}
+
+describe('getRequestUrl', function() {
+    let httpServer,
+        httpsServer;
+
+    before('Start HTTP server', function(done) {
+        httpServer = http.createServer((req, res) => res.end());
+        httpServer.on('listening', done);
+        httpServer.listen(HTTP_PORT);
+    });
+
+    before('Start HTTPS server', function(done) {
+        const options = {
+            key: fs.readFileSync('test/fixtures/localhost.key'),
+            cert: fs.readFileSync('test/fixtures/localhost.cert')
+        };
+        httpsServer = https.createServer(options, (req, res) => res.end());
+        httpsServer.on('listening', done);
+        httpsServer.listen(HTTPS_PORT);
+    });
+
+    const testUrls = [
+        `http://localhost:${HTTP_PORT}/`,
+        `https://localhost:${HTTPS_PORT}/`,
+        `http://localhost:${HTTP_PORT}/path/to/something.ext`,
+        `https://localhost:${HTTPS_PORT}/path/to/something.ext`,
+        `http://localhost:${HTTP_PORT}/path/to/something.ext?param1=abc&param2=def`,
+        `https://localhost:${HTTPS_PORT}/path/to/something.ext?param1=abc&param2=def`,
+        `http://username@localhost:${HTTP_PORT}/path/to/something.ext?param1=abc&param2=def`,
+        `https://username@localhost:${HTTPS_PORT}/path/to/something.ext?param1=abc&param2=def`,
+        `http://username:password@localhost:${HTTP_PORT}/path/to/something.ext?param1=abc&param2=def`,
+        `https://username:password@localhost:${HTTPS_PORT}/path/to/something.ext?param1=abc&param2=def`,
+    ];
+
+    testUrls.forEach(testUrl => {
+        [false, true].forEach(includeAuth => {
+            const expectedUrl = new URL(testUrl);
+
+            if (!includeAuth) {
+                expectedUrl.username = '';
+                expectedUrl.password = '';
+            }
+
+            const expectedUrlString = expectedUrl.toString();
+            const description = `should return ${expectedUrlString} when requesting ${testUrl}`
+                + ` (includeAuth=${includeAuth})`;
+
+            it(description, function(done) {
+                request(testUrl).then(res => {
+                    const requestUrl = getRequestUrl(res, includeAuth);
+                    assert.strictEqual(requestUrl.toString(), expectedUrlString);
+                    done();
+                });
+            });
+        });
+    });
+
+    after('Stop HTTP server', function(done) {
+        httpServer.on('close', done);
+        httpServer.close();
+    });
+
+    after('Stop HTTPS server', function(done) {
+        httpsServer.on('close', done);
+        httpsServer.close();
+    });
+});
